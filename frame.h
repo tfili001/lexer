028b269525fb4824098eb340b58cb2614311cf5b
tfili001 <tfili001@odu.edu>
2019-07-18 20:22:31 -0400

    reallign spacing

--- frame.h
+++ frame.h
@@ -7,11 +7,10 @@ using namespace std;
 
 struct frame
 {   
-
 	frame(){}
 	const char* fn_name;
     size_t arg_size;
-    
+
     vector<var> local_vars;
     vector<var>	output_stack;
 	var return_var;
