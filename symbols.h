de36a8365f54389907a8c19c5adb929b8d20576c
tfili001 <tfili001@odu.edu>
2019-07-20 19:40:16 -0400

    enum tokens moved to symbols.h

---
+++ symbols.h
@@ -0,0 +1,26 @@
+
+enum token_symbol
+{
+	IDENTIFIER,
+	EQUAL,
+	ADD,
+	L_PARA,
+	R_PARA,
+	STRING_LITERAL,
+	NUMERIC,
+	FN,
+	L_BRACE,
+	R_BRACE,
+	RETURN,
+	FUNCTION_CALL
+};
+
+	
+enum op_symbol
+{
+	OPERATOR,
+	OPERAND,
+	PARENTHESIS,
+	NO_SYMBOL
+};
+	
